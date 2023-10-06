2430e1c17ac99b0cbe6e3d231e89a2fae18868f0
Edmonds-Git <edmondst10@hotmail.com>
2023-10-06 15:47:16 +0100

    L2 EOS
    lab 2 end of session
    06/10/23

--- Tasks/Task-210-Struct/main.cpp
+++ Tasks/Task-210-Struct/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <cstdio>
 
 // Date Type ComplexNumber_C
 typedef struct {
@@ -23,7 +24,16 @@ int main() {
     // Create another complex number y
     // Calculate the complex sum of p and q and store in y
     // Use printf to display as a complex number (hint: you need two placeholders)
+    ComplexNumber_C y = {.real = 0.0, .imag = 0.0};
+
+   // ComplexNumber_C rr = {p.real + q.real};
+    //ComplexNumber_C jj = {p.imag + q.imag};
+
+    y.real = (p.real + q.real);
+    y.imag = (p.imag + q.imag);
     
+    printf("%f+j%f\n",y.real,y.imag);
+
     while (true) {
     }
 }
